2e31c37a291cd0f8889bdd717a2089386b35359c
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-10-25 18:00:02 +0900

    [Improve] add db operation

--- SpringBootSample/src/main/java/com/example/hello/HelloController.java
+++ SpringBootSample/src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+	@Autowired
+	private HelloService service;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		
@@ -20,5 +24,14 @@ public class HelloController {
 		
 		model.addAttribute("sample", str);
 		return "hello/response";
+	}
+	
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2") String id, Model model) {
+		
+		Employee employee = service.getEmployee(id);
+		model.addAttribute("employee", employee);
+		
+		return "hello/db";
 	}
 }
